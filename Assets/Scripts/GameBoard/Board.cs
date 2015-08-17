a28b80d4a2ac4d31d6695d8e0915fd132cbec904
Hampus Gustafsson <1992hg@gmail.com>
2015-08-17 19:58:42 +0200

    Added a class for Board which will be used to control board mechanics.

---
+++ Assets/Scripts/GameBoard/Board.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Board : MonoBehaviour {
+    public int Width;
+    public int Height;
+
+    private List<TileController> tiles;
+
+    private void Start () {
+        PopulateBoard();
+    }
+
+    private void Update () {
+    
+    }
+
+    public void PopulateBoard() {
+
+    }
+}
