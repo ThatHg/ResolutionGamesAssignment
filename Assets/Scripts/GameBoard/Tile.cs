441ef57984b9b2a933358b2788a2e2b5ac303d72
Hampus Gustafsson <1992hg@gmail.com>
2015-08-17 19:31:28 +0200

    Added tile class

---
+++ Assets/Scripts/GameBoard/Tile.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Tile : MonoBehaviour {
+    public enum State {
+        InvalidState = 0,
+        Static,
+        Moving,
+    };
+
+    public State CurrentState { get; private set; }
+
+    private void Start() {
+    
+    }
+
+    private void Update() {
+    
+    }
+}
