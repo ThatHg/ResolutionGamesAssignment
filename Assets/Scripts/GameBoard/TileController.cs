bc26966a29414b7280670a5a055f9f7b354a8e80
Hampus Gustafsson <1992hg@gmail.com>
2015-08-17 21:29:30 +0200

    Added properties for tile state and type, and started on some state management

--- Assets/Scripts/GameBoard/TileController.cs
+++ Assets/Scripts/GameBoard/TileController.cs
@@ -1,13 +1,26 @@
 ﻿using UnityEngine;
 
 public class TileController : MonoBehaviour {
+    public Tile.State state {
+        get { return tile.currentState; }
+        set { tile.currentState = value; }
+    }
+    public Tile.Type type {
+        get { return tile.type; }
+        set { tile.type = value; }
+    }
+
     private Tile tile;
 
-    void Start () {
+    private void Start () {
         
     }
 
-    void Update () {
+    private void Update () {
+        
+    }
 
+    private void OnCollisionStay() {
+        state = Tile.State.Static;
     }
 }
