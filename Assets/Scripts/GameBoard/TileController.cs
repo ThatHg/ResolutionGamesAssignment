330b64b33a11afe9ef73162426205bb552c6791b
Hampus Gustafsson <1992hg@gmail.com>
2015-08-18 18:17:15 +0200

    Removing Tile from it all

--- Assets/Scripts/GameBoard/TileController.cs
+++ Assets/Scripts/GameBoard/TileController.cs
@@ -2,7 +2,14 @@
 
 public class TileController : MonoBehaviour {
     private static int ID = 0;
-    public Tile.State state { get; set; }
+    public enum State
+    {
+        InvalidState = 0,
+        Static,
+        Moving,
+    };
+
+    public State state { get; set; }
 
     public int Id { get; private set; }
     public float speed;
@@ -19,7 +26,7 @@ public class TileController : MonoBehaviour {
     }
 
     private void Move() {
-        state = Tile.State.Moving;
+        state = State.Moving;
         var deltaSpeed = speed * Time.deltaTime;
         transform.position += ResolvMovement(deltaSpeed);
     }
@@ -29,7 +36,7 @@ public class TileController : MonoBehaviour {
 
         RaycastHit hit;
         if (Physics.Raycast(transform.position, -Vector3.up, out hit, deltaSpeed + tileCollider.bounds.extents.y)){
-            state = Tile.State.Static;
+            state = State.Static;
             var y0 = transform.position.y;
             var y1 = hit.collider.transform.position.y;
             var length = y0 - y1;
