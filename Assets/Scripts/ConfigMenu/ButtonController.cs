417197037ba17db0fefc72e0665a939ef62f0e44
Hampus Gustafsson <1992hg@gmail.com>
2015-08-18 20:57:17 +0200

    Added a button controller

---
+++ Assets/Scripts/ConfigMenu/ButtonController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ButtonController : MonoBehaviour {
+    public UnityEngine.UI.InputField widthField;
+    public UnityEngine.UI.InputField heightField;
+    public GameObject game;
+
+    public void ButtonPressed() {
+        int width;
+        int height;
+        int.TryParse(widthField.text, out width);
+        int.TryParse(heightField.text, out height);
+
+        var board = game.GetComponent<Board>();
+        Debug.Assert(board != null, "Error, ButtonController.cs - Could not find board component on gameobject");
+        board.SetWidth(width);
+        board.SetHeight(height);
+        board.ReBuildBoard();
+    }
+}
