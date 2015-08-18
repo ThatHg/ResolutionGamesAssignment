80b95fb6d84ba792af56e66fe66d4705b53e0163
Hampus Gustafsson <1992hg@gmail.com>
2015-08-18 18:14:24 +0200

    Added a zoom option

--- Assets/Scripts/Misc/ObjectInCamera.cs
+++ Assets/Scripts/Misc/ObjectInCamera.cs
@@ -2,11 +2,13 @@
 
 public class ObjectInCamera : MonoBehaviour {
     public Transform objectToEnclose;
+    public float zoom = 0.6f;
+
+    // This script is based on the formula from this website: http://stackoverflow.com/questions/21544336/how-to-position-the-camera-so-that-my-main-object-is-entirely-visible-and-fit-to
     void Update () {
         var renderer = objectToEnclose.GetComponent<Renderer>();
         Debug.Assert(renderer != null, "Error, ObjectInCamera.cs - Could not find objects renderer");
-
-        var radius = Mathf.Max(renderer.bounds.extents.y, renderer.bounds.extents.x) * 0.6f;
+        var radius = Mathf.Max(renderer.bounds.extents.y, renderer.bounds.extents.x) * zoom;
         var fieldOfView = Camera.main.fieldOfView;
         var z = radius / Mathf.Sin(fieldOfView);
         transform.position = new Vector3(0,0,z);
