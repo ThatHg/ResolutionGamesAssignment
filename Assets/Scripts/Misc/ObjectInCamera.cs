3034c7fe8f72549b7b32dbd48156ffbd1f7ef0cc
Hampus Gustafsson <1992hg@gmail.com>
2015-08-18 17:52:48 +0200

    Camera will not adjust itself to encompass whole game board

---
+++ Assets/Scripts/Misc/ObjectInCamera.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class ObjectInCamera : MonoBehaviour {
+    public Transform objectToEnclose;
+    void Update () {
+        var renderer = objectToEnclose.GetComponent<Renderer>();
+        Debug.Assert(renderer != null, "Error, ObjectInCamera.cs - Could not find objects renderer");
+
+        var radius = Mathf.Max(renderer.bounds.extents.y, renderer.bounds.extents.x) * 0.6f;
+        var fieldOfView = Camera.main.fieldOfView;
+        var z = radius / Mathf.Sin(fieldOfView);
+        transform.position = new Vector3(0,0,z);
+    }
+}
